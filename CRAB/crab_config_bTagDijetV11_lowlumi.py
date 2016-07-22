c9226b0a1b3ab275fa279ef2bfcf54a242460029
Artur <aavkhadi@gmail.com>
2016-07-22 14:25:34 +0200

    added crab config for lowlumi. cleaned up SimpleHLTAnalyzer

---
+++ CRAB/crab_config_bTagDijetV11_lowlumi.py
@@ -0,0 +1,47 @@
+############################
+#                          #
+#    JetHT Run 2016B       #
+#                          #
+############################
+
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+name = 'eff_bTagDijetV11_lowlumi' # will be part of the work area name and the storage subdir name
+runNom = '273158,273730,274161,274244'
+#runs and corresponding approximate instantaneous luminosities
+#273158 -- ~ 1.5e33
+#273503 -- ~ 2.0e33
+#273730 -- ~ 2.5e33
+#274161 -- ~ 3.0e33
+#274244 -- ~ 4.5e33
+
+# For information on config parameters, see
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile
+
+# section General
+config.General.requestName = name
+config.General.workArea = 'crab_test_' + name
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+# section JobType
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/cern.ch/user/a/aavkhadi/CMSSW_8_0_11/src/bbtoDijet/SimpleHLTAnalyzer/test/hlt_bTagDijetV11.py'
+config.JobType.outputFiles = ['hlt_bTagDijetV11.root']
+config.JobType.numCores = 16
+# section Data
+config.Data.inputDataset = '/JetHT/Run2016B-PromptReco-v2/AOD' # '/HLTPhysics/Run2016B-PromptReco-v2/AOD'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 3  # use crab submit --dryrun *.py to find optimal splitting
+config.Data.secondaryInputDataset = '/JetHT/Run2016B-v2/RAW'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt' # specifes good lumi sections to be used
+config.Data.totalUnits = -1 # analyze all events after applying the lumi mask
+config.Data.runRange = runNom
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False # no need to publish the results
+config.Data.outputDatasetTag = name
+config.Data.ignoreLocality = True
+
+# section Site
+config.Site.storageSite = 'T3_US_FNALLPC'
--- SimpleHLTAnalyzer/plugins/SimpleHLTAnalyzer.cc
+++ SimpleHLTAnalyzer/plugins/SimpleHLTAnalyzer.cc
@@ -63,9 +63,6 @@ class SimpleHLTAnalyzer : public edm::EDAnalyzer {
     HLTConfigProvider hltConfig_;
     std::string hltProcess_;
 
-    // Filter for accessing online kinematics
-    // edm::InputTag trigFilterJet_;
-
     // Tokens for trigger results, kinematics, and btags
     edm::EDGetTokenT<edm::TriggerResults>    trigresultsToken_;
     edm::EDGetTokenT<trigger::TriggerEvent>  trigsummaryToken_;
@@ -79,36 +76,17 @@ class SimpleHLTAnalyzer : public edm::EDAnalyzer {
  
     bool firstEvent_;
 
-    // hard coded EDM relic:
-    // create large arrays that will definitely fit 
-    // all variables for a given object 
-    // (e.g., pt's of a jet)
     const int maxResults_;
 
     unsigned int *passtrig_;
     // b-tagging discriminants
     int nbtagCSVOnlinejets_;
     int nbtagCSVOfflinejets_;
-    // leading jets
-    // float *bTagCSVOnline1_, *bTagCSVOnline2_, *bTagCSVOnline3_, *bTagCSVOnline4_;
-    // float *bTagCSVOffline1_, *bTagCSVOffline2_, *bTagCSVOffline3_, *bTagCSVOffline4_;
-    // arrays for sorting  
     float *bTagCSVOnline_;
     float *bTagCSVOffline_;
     // PFJet objects
     int npfjets_;
-    // leading jets
-    // float *pfjetpt1_, *pfjeteta1_, *pfjetphi1_, *pfjetmass1_;
-    // float *pfjetpt2_, *pfjeteta2_, *pfjetphi2_, *pfjetmass2_;
-    // float *pfjetpt3_, *pfjeteta3_, *pfjetphi3_, *pfjetmass3_;
-    // float *pfjetpt4_, *pfjeteta4_, *pfjetphi4_, *pfjetmass4_;
-    // arrays for sorting 
     float *pfjetpt_, *pfjeteta_, *pfjetphi_, *pfjetmass_;
-    // for debugging
-    int isptOrdered_;
-    // online kinematics
-    // int nhltjets_;
-    // float *hltjetpt_, *hltjeteta_, *hltjetphi_, *hltjetmass_;
 
     TTree* outTree_;
 
@@ -117,7 +95,6 @@ class SimpleHLTAnalyzer : public edm::EDAnalyzer {
 SimpleHLTAnalyzer::SimpleHLTAnalyzer(const edm::ParameterSet& iConfig) :
   // the assigned strings have to match the names in the config file  
   hltProcess_       (iConfig.getParameter<std::string>("hltProcess")),
-  // trigFilterJet_    (iConfig.getParameter<edm::InputTag>("trigFilterJet")),
   trigresultsToken_ (consumes<edm::TriggerResults>(iConfig.getParameter<edm::InputTag>("trigResults"))),
   trigsummaryToken_ (consumes<trigger::TriggerEvent>(iConfig.getParameter<edm::InputTag>("trigSummary"))),
   pfjetsToken_     (consumes<reco::PFJetCollection>(iConfig.getParameter<edm::InputTag>("pfJets"))),
@@ -131,59 +108,15 @@ SimpleHLTAnalyzer::SimpleHLTAnalyzer(const edm::ParameterSet& iConfig) :
 {
 
   passtrig_ = new unsigned int[maxResults_];
-  //btagging discriminants 
-  // 1st leading jet
-  // bTagCSVOnline1_ = 0;
-  // bTagCSVOffline1_ = 0;
-  // 2nd leading jet
-  // bTagCSVOnline2_ = 0;
-  // bTagCSVOffline3_ = 0;
-  // 3rd leading jet
-  // bTagCSVOnline3_ = 0;
-  // bTagCSVOffline3_ = 0;
-  // 4th leading jet
-  // bTagCSVOnline4_ = 0;
-  // bTagCSVOffline4_ = 0;
-  // btagging arrays 
   bTagCSVOnline_ = new float[maxResults_];
   bTagCSVOffline_ = new float[maxResults_];
   nbtagCSVOnlinejets_ = 0;
   nbtagCSVOfflinejets_ = 0;
-  // pfjet kinematics
   npfjets_ = 0;
-  // 1st leading jet
-  // pfjetpt1_ = 0;
-  // pfjeteta1_ = 0;
-  // pfjetphi1_ = 0;
-  // pfjetmass1_ = 0;
-  // 2nd leading jet
-  // pfjetpt2_ = 0;
-  // pfjeteta2_ = 0;
-  // pfjetphi2_ = 0;
-  // pfjetmass2_ = 0;
-  // 3rd leading jet
-  // pfjetpt3_ = 0;
-  //pfjeteta3_ = 0;
-  // pfjetphi3_ = 0;
-  // pfjetmass3_ = 0;
-  // 4th leading jet
-  // pfjetpt4_ = 0;
-  // pfjeteta4_ = 0;
-  // pfjetphi4_ = 0;
-  // pfjetmass4_ = 0;
-  // jet arrrays
   pfjetpt_ = new float[maxResults_];
   pfjeteta_ = new float[maxResults_];
   pfjetphi_ = new float[maxResults_];
   pfjetmass_ = new float[maxResults_];
- // for debugging 
-  isptOrdered_ = 1;
- // online info
- // nhltjets_ = 0;
- // hltjetpt_ = new float[maxResults_];
- // hltjeteta_ = new float[maxResults_];
- // hltjetphi_ = new float[maxResults_];
- // hltjetmass_ = new float[maxResults_];
 
   edm::Service<TFileService> fs;
   outTree_ = fs->make<TTree>("HLTAnalysis","");
@@ -195,45 +128,15 @@ SimpleHLTAnalyzer::SimpleHLTAnalyzer(const edm::ParameterSet& iConfig) :
   outTree_->Branch("run",  &run_,  "run/i");
   outTree_->Branch("lumi", &lumi_, "lumi/i");
   outTree_->Branch("evt",  &evt_,  "evt/i");
-//  outTree_->Branch("bTagCSVOnline1", bTagCSVOnline1_, "bTagCSVOnline1/F");
-//  outTree_->Branch("bTagCSVOnline2", bTagCSVOnline2_, "bTagCSVOnline2/F");
-//  outTree_->Branch("bTagCSVOnline3", bTagCSVOnline3_, "bTagCSVOnline3/F");
-//  outTree_->Branch("bTagCSVOnline4", bTagCSVOnline4_, "bTagCSVOnline4/F");
-//  outTree_->Branch("bTagCSVOffline1", bTagCSVOffline1_, "bTagCSVOffline1/F");
-//  outTree_->Branch("bTagCSVOffline2", bTagCSVOffline2_, "bTagCSVOffline2/F");
-//  outTree_->Branch("bTagCSVOffline3", bTagCSVOffline3_, "bTagCSVOffline3/F");
-//  outTree_->Branch("bTagCSVOffline4", bTagCSVOffline4_, "bTagCSVOffline4/F");
   outTree_->Branch("nbtagCSVOnlinejets", &nbtagCSVOnlinejets_, "nbtagCSVOnlinejets/I");
   outTree_->Branch("bTagCSVOnline",  bTagCSVOnline_, "bTagCSVOnline[nbtagCSVOnlinejets]/F"); 
   outTree_->Branch("nbtagCSVOfflinejets", &nbtagCSVOfflinejets_, "nbtagCSVOfflinejets/I");
   outTree_->Branch("bTagCSVOffline",  bTagCSVOffline_, "bTagCSVOffline[nbtagCSVOfflinejets]/F");
-//  outTree_->Branch("pfjetpt1", pfjetpt1_, "pfjetpt1/F");
-//  outTree_->Branch("pfjetpt2", pfjetpt2_, "pfjetpt2/F");
-//  outTree_->Branch("pfjetpt3", pfjetpt3_, "pfjetpt3/F");
-//  outTree_->Branch("pfjetpt4", pfjetpt4_, "pfjetpt4/F");  
-//  outTree_->Branch("pfjeteta1", pfjeteta1_, "pfjeteta1/F");
-//  outTree_->Branch("pfjeteta2", pfjeteta2_, "pfjeteta2/F");
-//  outTree_->Branch("pfjeteta3", pfjeteta3_, "pfjeteta3/F");
-//  outTree_->Branch("pfjeteta4", pfjeteta4_, "pfjeteta4/F");
-//  outTree_->Branch("pfjetphi1", pfjetphi1_, "pfjetphi1/F");
-//  outTree_->Branch("pfjetphi2", pfjetphi2_, "pfjetphi2/F");
-//  outTree_->Branch("pfjetphi3", pfjetphi3_, "pfjetphi3/F");
-//  outTree_->Branch("pfjetphi4", pfjetphi4_, "pfjetphi4/F");
-//  outTree_->Branch("pfjetmass1", pfjetmass1_, "pfjetmass1/F");
-//  outTree_->Branch("pfjetmass2", pfjetmass2_, "pfjetmass2/F");
-//  outTree_->Branch("pfjetmass3", pfjetmass3_, "pfjetmass3/F");
-//  outTree_->Branch("pfjetmass4", pfjetmass4_, "pfjetmass4/F");
-  outTree_->Branch("isptOrdered", &isptOrdered_, "isptOrdered/I");
   outTree_->Branch("npfjets",  &npfjets_,  "npfjets/I");
   outTree_->Branch("pfjetpt",  pfjetpt_,   "pfjetpt[npfjets]/F");
   outTree_->Branch("pfjeteta", pfjeteta_,   "pfjeteta[npfjets]/F");
   outTree_->Branch("pfjetphi", pfjetphi_,  "pfjetphi[npfjets]/F");
   outTree_->Branch("pfjetmass", pfjetmass_, "pfjetmass[npfjets]/F");
-//  outTree_->Branch("nhltjets",   &nhltjets_,  "nhltjets/I");
-//  outTree_->Branch("hltjetpt",   hltjetpt_,   "hltjetpt[nhltjets]/F");
-//  outTree_->Branch("hltjeteta",  hltjeteta_,  "hltjeteta[nhltjets]/F");
-//  outTree_->Branch("hltjetphi",  hltjetphi_,  "hltjetphi[nhltjets]/F");
-//  outTree_->Branch("hltjetmass", hltjetmass_, "hltjetmass[nhltjets]/F");
 }
 
 SimpleHLTAnalyzer::~SimpleHLTAnalyzer()
@@ -267,39 +170,12 @@ SimpleHLTAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 
   if(firstEvent_) firstEvent_ = false;
 
-//  edm::Handle<trigger::TriggerEvent> trigsummary;
-//  iEvent.getByToken(trigsummaryToken_, trigsummary);
-
-   // filtering by trigger path for the online objects
-   // correct trigger path ensures objects were created => exist 
-   // trigger::size_type trigFilterJet_Index       = trigsummary->filterIndex( trigFilterJet_ );
-   // trigger::TriggerObjectCollection triggerObjects = trigsummary->getObjects();
-
-//  if(trigFilterJet_Index < trigsummary->sizeFilters()) {
-//    const trigger::Keys& trigKeys = trigsummary->filterKeys(trigFilterJet_Index); 
-//    nhltjets_ = trigKeys.size();
-//    // need kinematics for up to 4 leading jets
-//    for(unsigned int ik = 0; ik < 4 && ik < trigKeys.size(); ++ik){ 
-//      const trigger::TriggerObject& obj = triggerObjects[trigKeys[ik]];
-//      hltjetpt_[ik] = obj.pt();
-//      hltjeteta_[ik] = obj.eta();
-//      hltjetphi_[ik] = obj.phi();
-//      hltjetmass_[ik] = obj.mass();
-//    }
-//  }
-
   // kinematics from PFJets
   edm::Handle<reco::PFJetCollection> pfjetHandle;
   iEvent.getByToken(pfjetsToken_, pfjetHandle);
   const reco::PFJetCollection & pfjets = *(pfjetHandle.product());
 
-  // want kinematics for up to 4 leading jets
-  //if (pfjets.size() > 4) {
-  //  npfjets_ = 4; 
-  //}
-  //else {
-    npfjets_ = pfjets.size();
-  //}
+  npfjets_ = pfjets.size();
   
   // loop over jets
   for(int i = 0; i != npfjets_; ++i) {
@@ -308,15 +184,6 @@ SimpleHLTAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
     pfjetphi_[i]  = pfjets[i].phi();
     pfjetmass_[i] = pfjets[i].mass();
   }
-
-  // check if pts are ordered
-  // if (npfjets_ > 1) {
-  //  for (int i = 1; i != npfjets_; ++i) {
-  //    if (pfjetpt_[i] > pfjetpt_[i-1]) {
-  //      isptOrdered_ = 0; 
-  //    }
-  //  }
-  //}
 
   // b-tagging discriminants
   
@@ -342,14 +209,7 @@ SimpleHLTAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
   iEvent.getByToken(btagsCSVOfflineToken_, btagCSVOfflineHandle);
   const reco::JetTagCollection & btagsCSVOffline = *(btagCSVOfflineHandle.product());
 
-  // want discriminants for up to 4 leading jets  
-  //if (btagsCSVOffline.size() > 4) {
-  //  nbtagCSVOfflinejets_ = 4; 
-  //}
-  //else {
-    nbtagCSVOfflinejets_ = btagsCSVOffline.size();
-  //}
-
+  nbtagCSVOfflinejets_ = btagsCSVOffline.size();
   //  loop over jets 
   for(int i = 0; i != nbtagCSVOfflinejets_; ++i) {
     bTagCSVOffline_[i] = btagsCSVOffline[i].second; // https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookBTagEdAnalyzer17X
