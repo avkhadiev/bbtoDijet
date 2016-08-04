203063337bc90e1ff6be7bc3bde8a9cc57fd32c2
Artur <aavkhadi@gmail.com>
2016-08-04 17:28:04 +0200

    add c++ correlation plotter with delta R matching, and test files

---
+++ bbtoDijetAnalyzer/test/makeCorrHistos.C
@@ -0,0 +1,182 @@
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include "TMath.h"
+#include "TFile.h"
+#include "TChain.h"
+#include "TString.h"
+#include "TH1F.h"
+#include "TGraphAsymmErrors.h"
+#include "TEfficiency.h"
+#include <vector>
+#include <string>
+#include "Style.hh"
+#endif
+
+float deltaR(float eta1, float phi1, float eta2, float phi2)
+{
+ float deltaPhi = TMath::Abs(phi1-phi2);
+ float deltaEta = eta1-eta2;
+ if(deltaPhi > TMath::Pi())
+   deltaPhi = TMath::TwoPi() - deltaPhi;
+ return TMath::Sqrt(deltaEta*deltaEta + deltaPhi*deltaPhi);
+}
+
+// Makes a turn-on curve once the histograms (with same binnings!) are ready
+// Error calculation: http://indico.cern.ch/event/109265/contributions/1312303/attachments/29409/42521/trigeff-20101019.pdf
+// also see https://root.cern.ch/doc/master/classTEfficiency.html
+TGraphAsymmErrors* makeEffGraph(TH1F* pass, TH1F* total, bool debug=false) {
+
+  // make sure <pass> and <total> have the same binning!
+  int npoints = total->GetNbinsX();
+  float x[npoints], y[npoints], errx[npoints], erryl[npoints], erryh[npoints];
+  float npass = 0.0;
+  float ntotal = 0.0;
+  for(int ibin = 1; ibin < npoints+1; ibin++) {
+    x[ibin-1] = total->GetBinCenter(ibin);
+    npass = pass->GetBinContent(ibin);
+    ntotal = total->GetBinContent(ibin);
+    y[ibin-1] = ntotal < 1.0 ? 0.0 : npass/ntotal; // signal efficiency zero if no events in the bin
+    errx[ibin-1] = 0.0; // uncertainty in parameters is zero
+    if(y[ibin-1] == 0.0) {
+    } else {
+      if(debug) printf("npass = %3.1f, ntotal = %3.1f, eff = %4.2f", npass, ntotal, y[ibin-1]);
+      // the thid argument in ClopperPearson is the confidence interval; 0.683 is the value recommended by the PDG
+      // see https://root.cern.ch/doc/master/classTEfficiency.html#ae80c3189bac22b7ad15f57a1476ef75b
+      // fourth parameter is a bool: is the value for the upper bound?
+      erryl[ibin-1] = y[ibin-1] - TEfficiency::ClopperPearson((unsigned int)ntotal, (unsigned int)npass, 0.683, false); 
+      erryh[ibin-1] = TEfficiency::ClopperPearson((unsigned int)ntotal, (unsigned int)npass, 0.683, true) - y[ibin-1]; 
+    }
+  }
+
+  TGraphAsymmErrors *gr = new TGraphAsymmErrors(npoints, x, y, errx, errx, erryl, erryh);
+
+  return gr;
+
+}
+
+void runTrigEffPlots(std::vector<TString> infileNames)
+{
+
+  const char * tagName   = "HLT_DoubleJetsC100_DoubleBTagCSV_p026_DoublePFJetsC160_v2";
+  const char * probeName = "HLT_DoubleJetsC100_p026_DoublePFJetsC160_v2"; 
+  TChain *chain = new TChain("bbtoDijet/efficiencyTree");
+
+  // add input file names
+  for(const auto fname : infileNames) {
+    chain->Add(fname);
+  }
+
+  int nevents = chain->GetEntries();
+  printf("Will process %d events\n",nevents);
+
+  // array sizes
+  const size_t kMaxTriggerPass     = 1000    ;
+  const size_t kMaxCaloJet         = 1000    ;
+  const size_t kMaxPFJet           = 1000    ;
+  const size_t kMaxBTagCSVOnline   = 1000    ;
+  const size_t kMaxBTagCSVOffline  = 1000    ;
+
+  // set of variables for 
+  // trigger-related information
+  unsigned int passTrigger                             = 0      ;    chain->SetBranchAddress(tagName,                    &passTrigger)                  ;
+  unsigned int passControl                             = 0      ;    chain->SetBranchAddress(probeName,                  &passControl)                  ;
+  // jet-related information 
+  // CaloJet 
+  int          caloJetCounter                          = 0      ;    chain->SetBranchAddress("caloJetCounter",           &caloJetCounter)               ;
+  float        caloJetEta[kMaxCaloJet]                          ;    chain->SetBranchAddress("caloJetEta",               &caloJetEta)                   ; 
+  float        caloJetPhi[kMaxCaloJet]                          ;    chain->SetBranchAddress("caloJetPhi",               &caloJetPhi)                   ;  
+  double       caloJetEt[kMaxCaloJet]                           ;    chain->SetBranchAddress("caloJetEt",                &caloJetEt)                    ;  
+  // b-jet-tagging information 
+  // CSVOnline
+  int          bTagCSVOnlineJetCounter                 = 0      ;    chain->SetBranchAddress("bTagCSVOnlineJetCounter",  &bTagCSVOnlineJetCounter)      ;
+  float        bTagCSVOnlineJetEta[kMaxBTagCSVOnline]           ;    chain->SetBranchAddress("bTagCSVOnlineJetEta",      &bTagCSVOnlineJetEta)          ; 
+  float        bTagCSVOnlineJetPhi[kMaxBTagCSVOnline]           ;    chain->SetBranchAddress("bTagCSVOnlineJetPhi",      &bTagCSVOnlineJetPhi)          ; 
+  double       bTagCSVOnlineJetEt[kMaxBTagCSVOnline]            ;    chain->SetBranchAddress("bTagCSVOnlineJetEt",       &bTagCSVOnlineJetEt)           ;  
+  float        bTagCSVOnline[kMaxBTagCSVOnline]                 ;    chain->SetBranchAddress("bTagCSVOnline",            &bTagCSVOnline)                ;
+  // CSVOffline
+  int          bTagCSVOfflineJetCounter                = 0      ;    chain->SetBranchAddress("bTagCSVOfflineJetCounter", &bTagCSVOfflineJetCounter)     ;
+  float        bTagCSVOfflineJetEta[kMaxBTagCSVOffline]         ;    chain->SetBranchAddress("bTagCSVOfflineJetEta",     &bTagCSVOfflineJetEta)         ; 
+  float        bTagCSVOfflineJetPhi[kMaxBTagCSVOffline]         ;    chain->SetBranchAddress("bTagCSVOfflineJetPhi",     &bTagCSVOfflineJetPhi)         ; 
+  double       bTagCSVOfflineJetEt[kMaxBTagCSVOffline]          ;    chain->SetBranchAddress("bTagCSVOfflineJetEt",      &bTagCSVOfflineJetEt)          ;  
+  float        bTagCSVOffline[kMaxBTagCSVOffline]               ;    chain->SetBranchAddress("bTagCSVOffline",           &bTagCSVOffline)               ;
+
+  // declare histograms
+  TH2D * correlationHisto1 = new TH2D("correlationHisto1", "Offline vs. Online CSV [Max];Online CSV;Offline CSV;Accepted",    50, 0, 1, 50, 0, 1);
+  TH2D * correlationHisto2 = new TH2D("correlationHisto1", "Offline vs. Online CSV [SubMax];Online CSV;Offline CSV;Accepted", 50, 0, 1, 50, 0, 1);
+
+  // Loop over all jets in the event
+  for(int ievent = 0; ievent < nevents; ievent++) {
+    chain->GetEntry(ievent);
+ 
+  //  loop over all jets in the event, store indices of jets with up to 8 highest Et values
+  //  double maxEt[8]  = {0.0}
+  //  int    iMaxEt[8] = {0}
+  //  for(int ijet = 0; ijet < caloJetCounter; ijet++) {
+  //      for(int iEt = 0; iEt < 8; iEt++) {
+  //          if(maxEt[iEt] < caloJetEt[ijet]) {
+  //              for(iShift = 7; iShift > iEt; iShift--) {
+  //                  iMaxEt[iShift] = iMaxEt[iShift - 1]
+  //                  maxEt[iShift]  = maxEt[iShift - 1]
+  //              }
+  //              iMaxEt[iEt] = ijet
+  //              maxEt[iEt]  = caloJetEt[ijet]
+  //              break;
+  //          }
+  //      }
+        
+  // ****************************************************************************************
+    if(passControl) {
+        float maxCSVOnline[2]     = {-10.0, -10.0}  ;
+        int   iMaxCSVOnline[2]    = {0, 0}          ;
+        float matchCSVOffline[2]  = {-10.0, -10.0}  ;
+        int   iMatchCSVOffline[2] = {0, 0}          ;
+        // find max and submax online discriminants
+        for(int ijet = 0; ijet < bTagCSVOnlineJetCounter; ijet++) { 
+            if(maxCSVOnline[0] < bTagCSVOnline[ijet]) {
+                iMaxCSVOnline[1] = iMaxCSVOnline[0]    ;
+                iMaxCSVOnline[0] = ijet                ;
+                maxCSVOnline[1]  = maxCSVOnline[0]     ;
+                maxCSVOnline[0]  = bTagCSVOnline[ijet] ;
+            }
+            else if(maxCSVOnline[1] < bTagCSVOnline[ijet]){
+                iMaxCSVOnline[1] = ijet                ;
+                maxCSVOnline[1]  = bTagCSVOnline[ijet] ;
+            }
+        }
+        // match offline discriminants to max and submax online discriminants
+        for(int itag = 0; itag < 2; itag ++) {
+            float minDeltaR = 10.0  ;
+            for(int imatch = 0; imatch < bTagCSVOfflineJetCounter; imatch ++) {
+                float iDeltaR = deltaR(bTagCSVOnlineJetEta[iMaxCSVOnline[itag]], bTagCSVOnlineJetPhi[iMaxCSVOnline[itag]], bTagCSVOfflineJetEta[imatch], bTagCSVOfflineJetPhi[imatch]) ;
+                if(iDeltaR < minDeltaR){
+                    minDeltaR = iDeltaR;
+                    matchCSVOffline[itag]  = bTagCSVOffline[imatch] ;
+                    iMatchCSVOffline[itag] = imatch ; 
+                }
+            }
+            printf("minDeltaR = %f \n", minDeltaR);
+            printf("Online = %4.2f, Offline = %4.2f \n", maxCSVOnline[itag], matchCSVOffline[itag]);
+        }
+        correlationHisto1->Fill(maxCSVOnline[0], matchCSVOffline[0]) ;
+        correlationHisto2->Fill(maxCSVOnline[1], matchCSVOffline[1]) ;
+    }
+  }
+  // make plots
+  SetStyle();
+  TCanvas *c = MakeCanvas("corrCanvas","",800,600);
+  correlationHisto1->Draw("COLZ")                 ;
+  c->SaveAs("correlation_1.pdf")                  ;
+  correlationHisto2->Draw("COLZ")                 ;
+  c->SaveAs("correlation_2.pdf")                  ;
+}
+
+void makeCorrHistos()
+{
+
+  std::vector<TString> filelist;
+
+  // specify output tree
+  filelist.push_back("test_bTagDijetV11.root"); 
+
+  runTrigEffPlots(filelist);
+
+}
