eefae42a17f4d0d88907371f775e516208e3c569
Artur <aavkhadi@gmail.com>
2016-08-08 10:53:04 -0500

    add more work on estimateRate.C

--- bbtoDijetAnalyzer/macros/estimateRate.C
+++ bbtoDijetAnalyzer/macros/estimateRate.C
@@ -39,88 +39,104 @@ void makeGraph(std::vector<TString> infileNames)
   double       bTagCSVOnlineJetEt[kMaxBTagCSVOnline]            ;    chain->SetBranchAddress("bTagCSVOnlineJetEt",       &bTagCSVOnlineJetEt)           ;  
   float        bTagCSVOnline[kMaxBTagCSVOnline]                 ;    chain->SetBranchAddress("bTagCSVOnline",            &bTagCSVOnline)                ;
 
-  // declare histograms and graphs
-  
+  // declare histograms
+  TH2D * distributionCSV = new TH2D("distributionCSV", "Control p84;max online CSV;submax online CSV", 50, 0, 1, 50, 0, 1);
+      xBins = distribitionCSV->GetXaxis->GetNbins();
+      yBins = distributionCSV->GetYaxis->GetNbins();
+      printf("xBins = %d, yBins = %d \n", xBins, yBins);
+  graphSize = xBins * yBins;
+  printf("declaring graph with size %d \n", graphSize); 
+  TGraph2D rateGraph  = new TGraph2D( graphSize);  
+
   // Loop over all jets in the event
   for(int ievent = 0; ievent < nevents; ievent++) {
     chain->GetEntry(ievent);
- 
-    int iOrderedEt[bTagCSVOnlineJetCounter];
-    TMath::Sort(bTagCSVOnlineJetCounter, bTagCSVOnlineJetEt, iOrderedEt); // output array of indices corresponding to the decreasing order of pt 
-    int   njets                                 ;
-    float maxCSVOnline[2]     = {-10.0, -10.0}  ;
-    int   iMaxCSVOnline[2]    = {0, 0}          ;
+
     // want to only consider up to 6 highest Et jets to match online selection
+    int njets;
     if(bTagCSVOnlineJetCounter > 6){
     	njets = 6;
     }
     else{
         njets = bTagCSVOnlineJetCounter;
     }
-    // find max and submax online discriminants
-    // CSV tagger considers jets with Et > 30, CSV matcher considers jets with Et > 80. 
-    // to correct for bad input tag difference between CSV tagger and CSV matcher, enforce offline selection to 
-    // emulate the orrect online behaviour 
-    for(int ijet = 0; ijet < njets; ijet++) { 
-        if(bTagCSVOnlineJetEt[iOrderedEt[ijet]] > 80.0){
-            if(maxCSVOnline[0] < bTagCSVOnline[iOrderedEt[ijet]]) {
-                iMaxCSVOnline[1] = iMaxCSVOnline[0]    ;
-                iMaxCSVOnline[0] = iOrderedEt[ijet]                ;
-                maxCSVOnline[1]  = maxCSVOnline[0]     ;
-                maxCSVOnline[0]  = bTagCSVOnline[iOrderedEt[ijet]] ;
+
+    // need at least 2 b-tagged jets (other events passing the control have discriminant -10.0 and are not plotted)
+    if((njets > 1) && passControl){
+	
+        int iOrderedEt[bTagCSVOnlineJetCounter];
+        TMath::Sort(bTagCSVOnlineJetCounter, bTagCSVOnlineJetEt, iOrderedEt); // output array of indices corresponding to the decreasing order of et 
+        
+        float maxCSVOnline[2]     = {-10.0, -10.0}  ;
+        int   iMaxCSVOnline[2]    = {0, 0}          ;
+        
+        // find max and submax online discriminants
+        // CSV tagger considers jets with Et > 30, CSV matcher considers jets with Et > 80. 
+        // to correct for bad input tag difference between CSV tagger and CSV matcher, enforce offline selection to 
+        // emulate the orrect online behaviour 
+        for(int ijet = 0; ijet < njets; ijet++) { 
+            if(bTagCSVOnlineJetEt[iOrderedEt[ijet]] > 80.0){
+                if(maxCSVOnline[0] < bTagCSVOnline[iOrderedEt[ijet]]){
+                    iMaxCSVOnline[1] = iMaxCSVOnline[0]                ;
+                    iMaxCSVOnline[0] = iOrderedEt[ijet]                ;
+                    maxCSVOnline[1]  = maxCSVOnline[0]                 ;
+                    maxCSVOnline[0]  = bTagCSVOnline[iOrderedEt[ijet]] ;
+                }
+                else if(maxCSVOnline[1] < bTagCSVOnline[iOrderedEt[ijet]]){
+                    iMaxCSVOnline[1] = iOrderedEt[ijet]                            ;
+                    maxCSVOnline[1]  = bTagCSVOnline[iOrderedEt[ijet]]             ;
+                }
             }
-            else if(maxCSVOnline[1] < bTagCSVOnline[iOrderedEt[ijet]]){
-                iMaxCSVOnline[1] = iOrderedEt[ijet]                            ;
-                maxCSVOnline[1]  = bTagCSVOnline[iOrderedEt[ijet]]             ;
-            }
+            else { break; }
         }
-	else { break; }
-    }
-    // debugging 
-    if(ievent % 100 == 0) { 
-    printf("Max online = %4.2f, Submax online = %4.2f \n", maxCSVOnline[0], maxCSVOnline[1]);
-    printf("index in decreasing Et: iMax = %d, iSubmax = %d \n", iMaxCSVOnline[0], iMaxCSVOnline[1]);  
-    printf("corresponding Et: Max = %4.2f, Submax = %4.2f \n", bTagCSVOnlineJetEt[iMaxCSVOnline[0]], bTagCSVOnlineJetEt[iMaxCSVOnline[1]]);
-    }
-    // current control (probe with CSV matcher disabled) allows jets with Et in (30.0, 80.0); 
-    // to obtain adequate behavior, the search for max CSV was corrected to match the CSV filter selection
-    if(passControl){
-        effPlot->Fill(passTrigger, maxCSVOnline[0], maxCSVOnline[1]);
-	controlHisto->Fill(maxCSVOnline[0], maxCSVOnline[1]);	      
-	if(passTrigger){
-	   passHisto->Fill(maxCSVOnline[0], maxCSVOnline[1]);
+
+	// match offline discriminants to max and submax online discriminants -- if both had Et > 80.0 GeV
+        if(maxCSVOnline[1] > -10.0){ 
+	
+            // debugging 
+            if(ievent % 1000 == 0) { 
+               printf("********************************************* \n");
+	       printf("Max online = %4.2f, Submax online = %4.2f \n", maxCSVOnline[0], maxCSVOnline[1]);
+               printf("index in decreasing Et: iMax = %d, iSubmax = %d \n", iMaxCSVOnline[0], iMaxCSVOnline[1]);  
+               printf("corresponding Et: Max = %4.2f, Submax = %4.2f \n", bTagCSVOnlineJetEt[iMaxCSVOnline[0]], bTagCSVOnlineJetEt[iMaxCSVOnline[1]]);
+	    }
+
+	    // fill the histograms if online jets were well-matched with offline jets
+                if(ievent % 1000 == 0) {
+	           printf("Filling histograms and plots \n");
+		}
+                distributionCSV->Fill(MaxCSVOnline[0], MaxCSVOnline[1]);		
 	}
-	else {
-	   missHisto->Fill(maxCSVOnline[0], maxCSVOnline[1]);
-	}
-    }
+     }
+  }
+
+  printf("CSV distribution built! \n Estimating rate now... \n");
+
+  float xCoordinate, yCoordinate;
+  int value;
+  int i = 0;
+  for(x = 0; x < xBins; x++){
+      for(y = 0; y < yBins; y ++{
+          xCoordinate = distribitionCSV->GetXaxis()->GetBinCenter(x);
+          yCoordinate = distributionCSV->GetYaxis()->GetBinCenter(y);
+          value = distibutionCSV->Integral(x, xBins-1, y, yBins - 1);
+          // debugging 
+          printf("xCoordinate is %4.2f, yCoordinate is %4.2f \n", xCoordinate, yCoordinate) ;
+          printf("Calculated integral yields %d \n", value) ;
+          rateGraph->SetPoint(i, xCoordinate, yCoordinate, value) ;
+          i = ++i;
+      }
   }
 
   // make plots
   // write histograms as tree branches 
-  TFile outputFile("plotEfficiencies.root", "recreate");
-  outputFile.mkdir("efficiencies_");
-  outputFile.cd("efficiencies_");
-  effPlot->Write();
-  controlHisto->Write();
-  passHisto->Write();
-  missHisto->Write();	  
+  TFile outputFile("rate.root", "recreate");
+  outputFile.mkdir("rates");
+  outputFile.cd("rates");
+  distributionCSV->Write();
+  rateGraph->Write();
   outputFile.cd();
   outputFile.Close();
-
-  //gStyle->SetOptStat(0)                           ;
-  //int zMin, zMax                                  ; 
-  //TCanvas *c = MakeCanvas("corrCanvas","",800,600);
-  //zMin = ceil(0.0001 * correlationHisto1->GetEntries()) ; 
-  ////  zMax = ceil(0.1    * correlationHisto1->GetEntries()) ; 
-  //correlationHisto1->SetMinimum(zMin)             ;
-  //correlationHisto1->Draw("COLZ")                 ;
-  //c->SaveAs("correlation_1.pdf")                  ;
-  //zMin = ceil(0.0001 * correlationHisto2->GetEntries()) ; 
-  //// zMax = ceil(0.1    * correlationHisto2->GetEntries()) ; 
-  //correlationHisto2->Draw("COLZ")                 ;
-  //correlationHisto2->SetMinimum(zMin)             ;
-  //c->SaveAs("correlation_2.pdf")                  ;
 }
 
 void estimateRate()
